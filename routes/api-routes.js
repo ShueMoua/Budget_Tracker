3d11b54e7b9a3f2fc924f98f734417cc2bb1bb2d
Adam Neubauer <neub0099@gmail.com>
2020-11-03 13:07:39 -0600

    changes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -52,28 +52,51 @@ module.exports = function(app) {
       });
   });
 
-//   // DELETE route for deleting posts
-//   app.delete("/api/posts/:id", function(req, res) {
-//     db.Post.destroy({
-//       where: {
-//         id: req.params.id
-//       }
-//     })
-//       .then(function(dbPost) {
-//         res.json(dbPost);
-//       });
-//   });
+  // DELETE route for deleting posts
+  app.delete("/api/income/:id", function(req, res) {
+    db.Income.destroy({
+      where: {
+        id: req.params.id
+      }
+    })
+      .then(function(dbPost) {
+        res.json(dbPost);
+      });
+  });
 
-//   // PUT route for updating posts
-//   app.put("/api/posts", function(req, res) {
-//     db.Post.update(req.body,
-//       {
-//         where: {
-//           id: req.body.id
-//         }
-//       })
-//       .then(function(dbPost) {
-//         res.json(dbPost);
-//       });
-//   });
+  app.delete("/api/expense/:id", function(req, res) {
+    db.Expense.destroy({
+      where: {
+        id: req.params.id
+      }
+    })
+      .then(function(dbPost) {
+        res.json(dbPost);
+      });
+  });
+
+  // PUT route for updating posts
+  app.put("/api/income", function(req, res) {
+    db.Income.update(req.body,
+      {
+        where: {
+          id: req.body.id
+        }
+      })
+      .then(function(dbPost) {
+        res.json(dbPost);
+      });
+  });
+
+  app.put("/api/expense", function(req, res) {
+    db.Expense.update(req.body,
+      {
+        where: {
+          id: req.body.id
+        }
+      })
+      .then(function(dbPost) {
+        res.json(dbPost);
+      });
+  });
 };
