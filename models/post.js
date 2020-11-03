f9e666d52a836bc4efb06859e17341b53c758021
Adam Neubauer <neub0099@gmail.com>
2020-11-03 10:44:21 -0600

    changes

---
+++ models/post.js
@@ -0,0 +1,24 @@
+module.exports = function(sequelize, DataTypes) {
+    var Income = sequelize.define("Income", {
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [1]
+        }
+      },
+      body: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+        validate: {
+          len: [1]
+        }
+      },
+      category: {
+        type: DataTypes.STRING,
+        defaultValue: "Personal"
+      }
+    });
+    return Post;
+  };
+  
