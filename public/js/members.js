ab02144d062d269ed5cfa406a69e5eb947a5f8b8
Shuelong Moua <moua_shuelong@yahoo.com>
2020-11-05 11:48:59 -0600

    creating chart

--- public/js/members.js
+++ public/js/members.js
@@ -2,6 +2,45 @@ $(document).ready(function() {
     // This file just does a GET request to figure out which user is logged in
     // and updates the HTML on the page
     $.get("/api/user_data").then(function(data) {
-        $(".member-name").text(data.email);
+        $(".member-name").text("Welcome" + data.email);
+        console.log(data);
+        console.log(data.email)
+        console.log(data.Expenses)
+        console.log(data.Expenses[0].amount)
+        const incExpense = data.Expenses[0].amount
+        console.log("this is const inc" + incExpense)
     });
+
+
+    const ctx = document.getElementById('myChart').getContext('2d');
+    const myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: ['Something', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'], //use ajax call and grab the description from each table, and insert the variable here(this is the x-axis and will be the name of the data within the chart)
+            datasets: [{
+                //your own label here
+                label: 'Expenses',
+                data: [12, 19, 3, 5, 2, 3], //use ajax call and grab the amount and insert it the variable here(this is the y-axis)
+                fill: false,
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        },
+
+    });
+
 });
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -3,152 +3,154 @@ var db = require("../models");
 var passport = require("../config/passport");
 
 module.exports = function(app) {
-  // Using the passport.authenticate middleware with our local strategy.
-  // If the user has valid login credentials, send them to the members page.
-  // Otherwise the user will be sent an error
-  app.post("/api/login", passport.authenticate("local"), function(req, res) {
-    res.json(req.user);
-  });
+    // Using the passport.authenticate middleware with our local strategy.
+    // If the user has valid login credentials, send them to the members page.
+    // Otherwise the user will be sent an error
+    app.post("/api/login", passport.authenticate("local"), function(req, res) {
+        res.json(req.user);
+    });
 
-  // Route for signing up a user. The user's password is automatically hashed and stored securely thanks to
-  // how we configured our Sequelize User Model. If the user is created successfully, proceed to log the user in,
-  // otherwise send back an error
-  app.post("/api/signup", function(req, res) {
-    db.User.create({
-      email: req.body.email,
-      password: req.body.password
-    })
-      .then(function() {
-        res.redirect(307, "/api/login");
-      })
-      .catch(function(err) {
-        res.status(401).json(err);
-      });
-  });
+    // Route for signing up a user. The user's password is automatically hashed and stored securely thanks to
+    // how we configured our Sequelize User Model. If the user is created successfully, proceed to log the user in,
+    // otherwise send back an error
+    app.post("/api/signup", function(req, res) {
+        db.User.create({
+                email: req.body.email,
+                password: req.body.password
+            })
+            .then(function() {
+                res.redirect(307, "/api/login");
+            })
+            .catch(function(err) {
+                res.status(401).json(err);
+            });
+    });
 
-  // Route for logging user out
-  app.get("/logout", function(req, res) {
-    req.logout();
-    res.redirect("/");
-  });
+    // Route for logging user out
+    app.get("/logout", function(req, res) {
+        req.logout();
+        res.redirect("/");
+    });
 
-  // Route for getting some data about our user to be used client side
-  app.get("/api/user_data", function(req, res) {
-    if (!req.user) {
-      // The user is not logged in, send back an empty object
-      res.json({});
-    } else {
-      // Otherwise send back the user's email and id
-      // Sending back a password, even a hashed password, isn't a good idea
-      res.json({
-        email: req.user.email,
-        id: req.user.id
-      });
-    }
-  });
-
-  app.get("/api/expense", function(req, res) {
-    db.Expense.findAll({
-      where: {
-        UserId: req.user.id
-      },
-      order: [
-        ['day','DESC']
-      ]
-    })
-      .then(function(dbPost) {
-        res.json(dbPost);
-      });
-  });
-
-   // GET route for getting all of the incomes
-   app.get("/api/income", function(req, res) {
-    db.Income.findAll({
-      where: {
-        UserId: req.user.id
-      },
-      order: [
-        ['day','DESC']
-      ]
-    })
-      .then(function(dbPost) {
-        res.json(dbPost);
-      });
-  });
-
-  // POST route for saving a new expense
-  app.post("/api/expense", function(req, res) {
-    console.log(req.body);
-    db.Expense.create({
-      amount: req.body.amount,
-      description: req.body.description,
-      day: req.body.day,
-      UserId: req.user.id
-    })
-      .then(function(dbPost) {
-        res.json(dbPost);
-      });
-  });
-
-  app.post("/api/income", function(req, res) {
-    console.log(req.body);
-    db.Income.create({
-      amount: req.body.amount,
-      description: req.body.description,
-      day: req.body.day,
-      UserId: req.user.id
-    })
-      .then(function(dbPost) {
-        res.json(dbPost);
-      });
-  });
-
-  app.delete("/api/income/:id", function(req, res) {
-    db.Income.destroy({
-      where: {
-        id: req.params.id
-      }
-    })
-      .then(function(dbPost) {
-        res.json(dbPost);
-      });
-  });
-
-  app.delete("/api/expense/:id", function(req, res) {
-    db.Expense.destroy({
-      where: {
-        id: req.params.id
-      }
-    })
-      .then(function(dbPost) {
-        res.json(dbPost);
-      });
-  });
-
-  // PUT route for updating posts
-  app.put("/api/income", function(req, res) {
-    db.Income.update(req.body,
-      {
-        where: {
-          id: req.body.id
+    // Route for getting some data about our user to be used client side
+    app.get("/api/user_data", function(req, res) {
+        if (!req.user) {
+            // The user is not logged in, send back an empty object
+            res.json({});
+        } else {
+            db.User.findOne({
+                where: { id: req.user.id },
+                include: [{
+                        model: db.Income
+                    },
+                    {
+                        model: db.Expense
+                    }
+                ]
+            }).then(function(data) {
+                res.json(data)
+            })
         }
-      })
-      .then(function(dbPost) {
-        res.json(dbPost);
-      });
-  });
+    });
 
-  app.put("/api/expense", function(req, res) {
-    db.Expense.update(req.body,
-      {
-        where: {
-          id: req.body.id
-        }
-      })
-      .then(function(dbPost) {
-        res.json(dbPost);
-      });
-  });
+    app.get("/api/expense", function(req, res) {
+        db.Expense.findAll({
+                where: {
+                    UserId: req.user.id
+                },
+                order: [
+                    ['day', 'DESC']
+                ]
+            })
+            .then(function(dbPost) {
+                res.json(dbPost);
+            });
+    });
+
+    // GET route for getting all of the incomes
+    app.get("/api/income", function(req, res) {
+        db.Income.findAll({
+                where: {
+                    UserId: req.user.id
+                },
+                order: [
+                    ['day', 'DESC']
+                ]
+            })
+            .then(function(dbPost) {
+                res.json(dbPost);
+            });
+    });
+
+    // POST route for saving a new expense
+    app.post("/api/expense", function(req, res) {
+        console.log(req.body);
+        db.Expense.create({
+                amount: req.body.amount,
+                description: req.body.description,
+                day: req.body.day,
+                UserId: req.user.id
+            })
+            .then(function(dbPost) {
+                res.json(dbPost);
+            });
+    });
+
+    app.post("/api/income", function(req, res) {
+        console.log(req.body);
+        db.Income.create({
+                amount: req.body.amount,
+                description: req.body.description,
+                day: req.body.day,
+                UserId: req.user.id
+            })
+            .then(function(dbPost) {
+                res.json(dbPost);
+            });
+    });
+
+    app.delete("/api/income/:id", function(req, res) {
+        db.Income.destroy({
+                where: {
+                    id: req.params.id
+                }
+            })
+            .then(function(dbPost) {
+                res.json(dbPost);
+            });
+    });
+
+    app.delete("/api/expense/:id", function(req, res) {
+        db.Expense.destroy({
+                where: {
+                    id: req.params.id
+                }
+            })
+            .then(function(dbPost) {
+                res.json(dbPost);
+            });
+    });
+
+    // PUT route for updating posts
+    app.put("/api/income", function(req, res) {
+        db.Income.update(req.body, {
+                where: {
+                    id: req.body.id
+                }
+            })
+            .then(function(dbPost) {
+                res.json(dbPost);
+            });
+    });
+
+    app.put("/api/expense", function(req, res) {
+        db.Expense.update(req.body, {
+                where: {
+                    id: req.body.id
+                }
+            })
+            .then(function(dbPost) {
+                res.json(dbPost);
+            });
+    });
 };
-
-
